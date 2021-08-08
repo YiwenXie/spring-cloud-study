62a721d1e7ec0f1b1a2ce29947b2a9126912a0ba
ywxie <30338551+Yiwenzxyzhh@users.noreply.github.com>
2021-08-08 10:39:41 +0800

    eureka集群环境构建

---
+++ cloud-eureka-server7002/src/main/java/com/atyiwen/springcloud/EurekaMain7002.java
@@ -0,0 +1,19 @@
+package com.atyiwen.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author ywxie
+ * @date 2021/8/8 9:44
+ * @describe 服务注册中心主启动类
+ * @EnableEurekaServer: 代表此类为Eureka服务注册中心
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaMain7002 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaMain7002.class, args);
+    }
+}
