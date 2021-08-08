8f6267e3004a35e6f979791cd3f06674d91350ac
ywxie <30338551+Yiwenzxyzhh@users.noreply.github.com>
2021-08-08 10:00:22 +0800

    EurekaServer初始配置+支付微服务8001入驻Eureka

--- cloud-provider-payment8001/src/main/java/com/atyiwen/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/atyiwen/springcloud/PaymentMain8001.java
@@ -2,6 +2,7 @@ package com.atyiwen.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author ywxie
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @describe 微服务提供者启动类
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
